9ff1b70ec4dc496e92f09f35f0b44559f70e001e
666vasilyev <vasilyev.jpg@yandex.ru>
2024-12-16 17:53:51 +0300

    new text for analysis

--- src/main/java/com/example/duplicate/Main.java
+++ src/main/java/com/example/duplicate/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
         String text1 = "Пример текста для анализа.";
-        String text2 = "Пример текста для анализа.";
+        String text2 = "Пример текста для анализаfff.";
 
         DuplicateDetector detector = new DuplicateDetector(200);
         double similarity = detector.compareTexts(text1, text2, 10);
