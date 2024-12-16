eb9e35fe17cf208e982a543c4da11e044f747c38
666vasilyev <vasilyev.jpg@yandex.ru>
2024-12-16 17:52:07 +0300

    use new method

--- src/main/java/com/example/duplicate/DuplicateDetector.java
+++ src/main/java/com/example/duplicate/DuplicateDetector.java
@@ -1,5 +1,7 @@
 package com.example.duplicate;
 
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.Set;
 
 // поиск дубликатов
@@ -10,27 +12,30 @@ public class DuplicateDetector {
     public DuplicateDetector(int numHashes) {
         this.minHasher = new MinHasher(numHashes);
     }
+    private double jaccardSimilarity(Set<String> shingles1, Set<String> shingles2) {
+        Set<String> intersection = new HashSet<>(shingles1);
+        intersection.retainAll(shingles2); // Пересечение
+
+        Set<String> union = new HashSet<>(shingles1);
+        union.addAll(shingles2); // Объединение
+
+        return (double) intersection.size() / union.size(); // Коэффициент
+    }
 
     public double compareTexts(String text1, String text2, int shingleSize) {
+        // Канонизация текстов
         String canonicalText1 = Canonicalizer.canonicalize(text1);
         String canonicalText2 = Canonicalizer.canonicalize(text2);
 
+        // Генерация шинглов
         Set<String> shingles1 = Shingler.generateShingles(canonicalText1, shingleSize);
         Set<String> shingles2 = Shingler.generateShingles(canonicalText2, shingleSize);
 
-        int[] minHashes1 = minHasher.computeMinHashes(shingles1);
-        int[] minHashes2 = minHasher.computeMinHashes(shingles2);
+        // Вычисление Jaccard-коэффициента для шинглов
+        double jaccardSimilarity = jaccardSimilarity(shingles1, shingles2);
 
-        return jaccardSimilarity(minHashes1, minHashes2);
+        return jaccardSimilarity; 
     }
 
-    private double jaccardSimilarity(int[] hashes1, int[] hashes2) {
-        int intersection = 0;
-        for (int i = 0; i < hashes1.length; i++) {
-            if (hashes1[i] == hashes2[i]) {
-                intersection++;
-            }
-        }
-        return (double) intersection / hashes1.length;
-    }
+
 }
